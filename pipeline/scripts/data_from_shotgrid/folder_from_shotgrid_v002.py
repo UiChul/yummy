a136c22854a1640a1d781956a02e6e6b4ba733b2
tourss <dlgyrl5759@gmail.com>
2024-08-19 10:07:02 +0900

    08.19 new

--- pipeline/scripts/data_from_shotgrid/folder_from_shotgrid_v002.py
+++ pipeline/scripts/data_from_shotgrid/folder_from_shotgrid_v002.py
@@ -8,96 +8,67 @@ API_KEY = "ljbgffxqg@cejveci5dQebhdx"
 
 # base path
 base_path = "/home/rapa/YUMMY"
-project_name = "Yummy"
+project_name = "Marvelous"
 
 def connect_sg():
     global sg
-    sg = shotgun.Shotgun(URL,
-                         SCRIPT_NAME,
-                         API_KEY)
+    sg = shotgun.Shotgun(URL, SCRIPT_NAME, API_KEY)
 
     return sg
 
-def get_project_id(project_name):
-    sg = connect_sg()
+def get_project_id(sg, project_name):
+    """
+    프로젝트 이름을 키워드로 프로젝트 id 가져오기
+    """
 
     filters = [["name", "is", project_name]]
     fields = ["id"]
     project = sg.find_one("Project", filters=filters, fields=fields)
-
+ 
     return project["id"]
 
-def get_sequences_from_project(project_name):
+def get_sequences_from_project(sg, project_id):
     """
-    프로젝트 이름을 키워드로 시퀀스를 가져옴
+    프로젝트 id를 키워드로 시퀀스를 가져오기
     """
-    sg = connect_sg()
-    project_id = get_project_id(project_name)
 
-    if not project_id:
-        return []
-
-    filters = [["project.Project.name", "is", project_name]]
+    filters = [["project", "is", {"type": "Project", "id": project_id}]]
     fields = ["id", "code"]
     sequences = sg.find("Sequence", filters=filters, fields=fields)
-
+ 
     return sequences
 
-# def get_shots_from_project(project_name):
-#     """
-#     프로젝트 이름을 키워드로 시퀀스에 연결된 샷들을 가져옴
-#     """
-#     sg = connect_sg()
-#     project_id = get_project_id(project_name)
-
-#     if not project_id:
-#         return []
-
-#     filters = [["project.Project.name", "is", project_name]]
-#     fields = ["id","code"]
-#     shots = sg.find("Shot", filters=filters, fields=fields)
-
-#     return shots
-
-def get_shots_from_sequence(sequence):
+def get_shots_from_sequence(sg, sequence):
     """
     특정 시퀀스에서 샷 가져오기
     """
-    sg = connect_sg()
-
     sequence_id = sequence["id"]
 
     filters = [["sg_sequence", "is", {"type": "Sequence", "id": sequence_id}]]
     fields = ["id", "code", "sg_sequence.Sequence.code"]
-    
     shots = sg.find("Shot", filters=filters, fields=fields)
 
     return shots
 
-def get_tasks_from_project(project_name):
+def get_tasks_from_project(sg, project_id):
     """
-    프로젝트 이름 키워드로 tasks를 가져옴
+    프로젝트 이름 키워드로 tasks 가져오기
     tasks로 steps(팀 이름)을 가져오기 위함
     """
-    sg = connect_sg()
-    project_id = get_project_id(project_name)
 
     if not project_id:
         return []
     
     filters = [["project", "is", {"type": "Project", "id": project_id}]]
     fields = ["id", "content", "step.Step.short_name"]  # "task_assignees.HumanUser.name"
-
     tasks = sg.find("Task", filters=filters, fields=fields)
 
     return tasks
 
-def get_steps_from_tasks(project_name):
+def get_steps_from_tasks(tasks):
     """
-    tasks의 데이터 중에서 step을 가져옴
+    tasks의 데이터 중에서 steps(팀 이름) 가져오기
     """
-
-    tasks = get_tasks_from_project(project_name)
     steps = set()
 
     for task in tasks:
@@ -107,7 +78,17 @@ def get_steps_from_tasks(project_name):
 
     return steps
 
-def generate_project_folders(project_name, sequences, steps):
+def get_asset_types_from_project(sg, project_id):
+    """
+    프로젝트 id 키워드로 asset_type 가져오기
+    """
+    filters = [["project", "is", {"type": "Project", "id": project_id}]]
+    fields = ["id", "code", "sg_asset_type"]
+    asset_types = sg.find("Asset", filters=filters, fields=fields)
+    
+    return asset_types
+
+def generate_project_folders(project_name, sequences, steps, asset_types):
 
 # folder_name
     exr_folder = "exr"
@@ -141,7 +122,7 @@ def generate_project_folders(project_name, sequences, steps):
     pipeline_folder = "pipeline"
     scripts_folder = "scripts"
 
-# {I/O}_{asset}_{pipeline}_folder_structure
+# {I/O}, {asset}, {pipeline} folder_structure
     folders = [
         f"project/{project_name}/{IO_folder}/{input_folder}/{plate_folder}/{exr_folder}",
         f"project/{project_name}/{IO_folder}/{input_folder}/{plate_folder}/{mov_folder}",
@@ -151,32 +132,27 @@ def generate_project_folders(project_name, sequences, steps):
         f"project/{project_name}/{tem_folder}/{shot_folder}/{clip_lib_foler}",
         f"project/{project_name}/{tem_folder}/{shot_folder}/{node_tem_foler}",
 
-        f"project/{project_name}/{asset_folder}/{cha_folder}/{dev_folder}",
-        f"project/{project_name}/{asset_folder}/{cha_folder}/{pub_folder}",
-        f"project/{project_name}/{asset_folder}/{env_folder}/{dev_folder}",
-        f"project/{project_name}/{asset_folder}/{env_folder}/{pub_folder}",
-        f"project/{project_name}/{asset_folder}/{prop_folder}/{dev_folder}",
-        f"project/{project_name}/{asset_folder}/{prop_folder}/{pub_folder}",
-        f"project/{project_name}/{asset_folder}/{weapon_folder}/{dev_folder}",
-        f"project/{project_name}/{asset_folder}/{weapon_folder}/{pub_folder}",
-        f"project/{project_name}/{asset_folder}/{matte_folder}/{dev_folder}",
-        f"project/{project_name}/{asset_folder}/{matte_folder}/{pub_folder}",
-
         f"{pipeline_folder}/{scripts_folder}"
         ]
+    
+#
+    for asset_type in asset_types:
+        asset_type = asset_type.get("sg_asset_type")
+        
+        folders.append(f"project/{project_name}/{asset_folder}/{asset_type}")
 
 # {seq}_{shot}_{step}_folder_structure
-    for seq in sequences:
-        seq_name = seq.get("code")
+    for sequence in sequences:
+        seq_name = sequence.get("code")
         
-        shots = get_shots_from_sequence(seq)
+        shots = get_shots_from_sequence(sg, sequence)
         
         for shot in shots:
             shot_name = shot.get("code")
             for step_name in steps:
                 folders.append(f"project/{project_name}/{seq_folder}/{seq_name}/{shot_name}/{step_name}/dev")
                 folders.append(f"project/{project_name}/{seq_folder}/{seq_name}/{shot_name}/{step_name}/pub")
-
+    
     return folders
 
 # create or update folder
@@ -185,14 +161,23 @@ def create_or_update_folders(base_path, folders):
         path = os.path.join(base_path, folder)
         
         if os.path.exists(path):
-            print(f"Folder already exists: {path}")
+            print(f"{path} 폴더가 이미 존재합니다")
         else:
             os.makedirs(path, exist_ok=True)
-            print(f"Created: {path}")
+            print(f"{path} 폴더가 생성되었습니다")
 
-# 변수 및 폴더 생성 스크립트 실행
-sequences = get_sequences_from_project(project_name)
-steps = get_steps_from_tasks(project_name) #steps = 각 팀이름
-
-folders = generate_project_folders(project_name, sequences, steps)
-create_or_update_folders(base_path, folders)
+if __name__ == "__main__":
+    sg = connect_sg()
+    # 프로젝트 id 한 번만 가져오기
+    project_id = get_project_id(sg, project_name)
+    
+    if project_id:
+        sequences = get_sequences_from_project(sg, project_id)
+        tasks = get_tasks_from_project(sg, project_id)
+        steps = get_steps_from_tasks(tasks)
+        asset_types = get_asset_types_from_project(sg, project_id)
+        # 폴더 생성 및 업데이트
+        folders = generate_project_folders(project_name, sequences, steps, asset_types)
+        create_or_update_folders(base_path, folders)
+    else:
+        print(f"'{project_name}'를 찾을 수 없습니다")
