49816ed6b12dc904964034e02da0274e33cef1c4
UiChul <suc708@naver.com>
2024-08-19 09:44:32 +0900

    loader_v003

--- pipeline/scripts/loader/main.py
+++ pipeline/scripts/loader/main.py
@@ -2,17 +2,41 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QWidget,QApplication,QMessageBox
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
+import os
 
 class Mainloader(QWidget):
     def __init__(self,info):
         super().__init__()
         self.set_up()
-        self.info = info
-        self.set_main_laoder()
         
+        self.project = info["project"]
+        self.user = info["name"]
+        
+        self.set_main_laoder()
+        self.set_comboBox_seq()
+        
+        
+        #Signal
+        self.ui.comboBox_seq.currentTextChanged.connect(self.set_comboBox_shot)
+        
+        
+    def set_comboBox_seq(self):
+        file_path = f"/home/rapa/YUMMY/project/{self.project}/seq"
+        seq_list = os.listdir(file_path)
+        self.ui.comboBox_seq.addItems(seq_list)
+    
+    def set_comboBox_shot(self,seq):
+        self.ui.comboBox_shot_shotname.clear()
+        file_path = f"/home/rapa/YUMMY/project/{self.project}/seq/{seq}"
+        shot_list = os.listdir(file_path)
+        self.ui.comboBox_shot_shotname.addItems(shot_list)
+
+        
+    
     def set_main_laoder(self):
-        self.ui.label_projectname.setText(f"{self.info["project"]}")
-        self.ui.label_username.setText(f"{self.info["name"]}")
+    
+        self.ui.label_projectname.setText(f"{self.project}")
+        self.ui.label_username.setText(f"{self.user}")
         
     def set_up(self):
         from main_window_ui import Ui_Form
@@ -24,8 +48,10 @@ class Mainloader(QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
+info = {"project" : "YUMMY" , "name" : "su"}
+
 if __name__ == "__main__":
     app = QApplication()
-    my  = Mainloader(info=3)
+    my  = Mainloader(info)
     my.show()
     app.exec()
