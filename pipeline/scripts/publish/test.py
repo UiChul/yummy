b44c8a2cf4e826c8b488f4e6853532ab4b84403b
jiyeon250 <stellalee969@gmail.com>
2024-08-26 14:18:01 +0900

    0826_exr적용전

--- pipeline/scripts/publish/test.py
+++ pipeline/scripts/publish/test.py
@@ -1,12 +1,7 @@
+new_path = []
+nk_new_path = "a"
+exr_new_path = "b"
+mov_new_path = "c"
+new_path.extend([nk_new_path, exr_new_path, mov_new_path])
 
-import os
-
-self.increase_version("C:/Users/LEE JIYEON/Desktop/YUMMY/project/Marvelous/seq/OPN/OPN_0010/cmp/dev/work/test0825_v001.nknc")
-
-def increase_version(self, file_path):
-        """
-        파일 경로 받아서 버전업하는 함수
-        """
-        base, ext = os.path.splitext(file_path)
-        print(base)
-        print(ext)
+print(new_path)
